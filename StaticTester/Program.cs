bbb92de8cefbe24b4c52b42f0b51f357049e0331
Sreelal.ES <sreethered@gmail.com>
2019-09-24 12:25:48 +0530

    Testing

---
+++ StaticTester/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = Console.ReadLine();
+            Console.WriteLine(msg);
+        }
+    }
+}
